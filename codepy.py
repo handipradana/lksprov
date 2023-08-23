61e5df1524194cccd0d3b22fcd388b9320a89ea9
handipradana <113164329+handipradana@users.noreply.github.com>
2023-08-23 11:34:56 +0700

    Update codepy.py

--- codepy.py
+++ codepy.py
@@ -1,24 +1,25 @@
-import os
 import json
 import boto3
 import paho.mqtt.client as mqtt
 from datetime import datetime
 from decimal import Decimal
+from dotenv import load_dotenv
+import os
 
-mqtt_broker = os.environ.get('MQTT_BROKER')
-mqtt_port = int(os.environ.get('MQTT_PORT'))
-mqtt_topic = os.environ.get('MQTT_TOPIC')
+load_dotenv()  # Memuat nilai-nilai dari berkas .env ke dalam lingkungan
 
-aws_access_key = os.environ.get('AWS_ACCESS_KEY')
-aws_secret_key = os.environ.get('AWS_SECRET_KEY')
-s3_bucket = os.environ.get('S3_BUCKET')
-s3_region = os.environ.get('S3_REGION')
-
-dynamodb_table = os.environ.get('DYNAMODB_TABLE')
-dynamodb_region = os.environ.get('DYNAMODB_REGION')
-
-mqtt_username = os.environ.get('MQTT_USERNAME')
-mqtt_password = os.environ.get('MQTT_PASSWORD')
+mqtt_broker = os.getenv('MQTT_BROKER')
+mqtt_port = int(os.getenv('MQTT_PORT'))
+mqtt_topic = os.getenv('MQTT_TOPIC')
+aws_access_key = os.getenv('AWS_ACCESS_KEY')
+aws_secret_key = os.getenv('AWS_SECRET_KEY')
+aws_session_token = os.getenv('AWS_SESSION_TOKEN')
+s3_bucket = os.getenv('S3_BUCKET')
+s3_region = os.getenv('S3_REGION')
+dynamodb_table = os.getenv('DYNAMODB_TABLE')
+dynamodb_region = os.getenv('DYNAMODB_REGION')
+mqtt_username = os.getenv('MQTT_USERNAME')
+mqtt_password = os.getenv('MQTT_PASSWORD')
 
 def on_connect(client, userdata, flags, rc):
     print('Terhubung dengan broker MQTT')
@@ -34,10 +35,10 @@ def on_message(client, userdata, msg):
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     file_name = f'{topic}/{timestamp}.json'
 
-    s3_client = boto3.client('s3', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key, region_name=s3_region)
+    s3_client = boto3.client('s3', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key, aws_session_token=aws_session_token, region_name=s3_region)
     s3_client.put_object(Body=json.dumps(payload), Bucket=s3_bucket, Key=file_name)
 
-    dynamodb = boto3.resource('dynamodb', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key, region_name=dynamodb_region)
+    dynamodb = boto3.resource('dynamodb', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key, aws_session_token=aws_session_token, region_name=dynamodb_region)
     table = dynamodb.Table(dynamodb_table)
     payload['timestamp'] = timestamp
     payload['suhu'] = suhu
